7c825eb124ef9515c020497581d77eba8a311111
ERJoseJohnson <jose.johnson.jose@gmail.com>
2020-03-14 19:51:37 +0800

    feat: finsihed task 8

--- PA1/shellPrograms/shellCheckDaemon_code.c
+++ PA1/shellPrograms/shellCheckDaemon_code.c
@@ -10,27 +10,57 @@ int shellCheckDaemon_code()
    sprintf(command, "ps -efj | grep summond  | grep -v tty > output.txt");
 
    // TODO: Execute the command using system(command) and check its return value
-
+   int commandStatus = system(command);
    free(command);
+   if (commandStatus != -1)
+   {
 
-   int live_daemons = 0;
-   // TODO: Analyse the file output.txt, wherever you set it to be. You can reuse your code for countline program
-   // 1. Open the file
-   // 2. Fetch line by line using getline()
-   // 3. Increase the daemon count whenever we encounter a line
-   // 4. Close the file
-   // 5. print your result
+      int live_daemons = 0;
+      // TODO: Analyse the file output.txt, wherever you set it to be. You can reuse your code for countline program
+      // 1. Open the file
+      // 2. Fetch line by line using getline()
+      // 3. Increase the daemon count whenever we encounter a line
+      // 4. Close the file
+      // 5. print your result
 
-   if (live_daemons == 0)
-      printf("No daemon is alive right now\n");
+      int lineStatus;
+      char *tempMem = (char *)malloc(sizeof(char) * SHELL_BUFFERSIZE);
+      size_t lineChar = SHELL_BUFFERSIZE;
+
+      FILE *fp = fopen("output.txt", "r");
+
+      if (fp == NULL)
+      {
+         printf("The file did not open properly \n");
+         return 0;
+      }
+
+      lineStatus = getline(&tempMem, &lineChar, fp);
+      while (lineStatus > 0)
+      {
+         // printf("Number of char %d \n", lineStatus);
+         live_daemons++;
+         // printf("LineCount is %d \n", lineCount);
+         lineStatus = getline(&tempMem, &lineChar, fp);
+      }
+
+      fclose(fp);
+      free(tempMem);
+      printf("There are %d daemons running \n", live_daemons);
+
+      if (live_daemons == 0)
+         printf("No daemon is alive right now\n");
+      else
+      {
+         printf("There are in total of %d live daemons \n", live_daemons);
+      }
+
+      // TODO: close any file pointers and free any statically allocated memory
+   }
    else
    {
-      printf("There are in total of %d live daemons \n", live_daemons);
+      printf("The command did not go through \n");
    }
-
-
-   // TODO: close any file pointers and free any statically allocated memory 
-
    return 1;
 }
 
